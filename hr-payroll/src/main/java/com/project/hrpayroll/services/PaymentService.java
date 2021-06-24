eaa83980a3f58f164a44e55519aafac53baafa58
Newton Neto <newton.mgs@gmail.com>
2021-06-24 10:26:24 -0300

    Comunicacao entre servicos

---
+++ hr-payroll/src/main/java/com/project/hrpayroll/services/PaymentService.java
@@ -0,0 +1,29 @@
+package com.project.hrpayroll.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.project.hrpayroll.entities.Payment;
+import com.project.hrpayroll.entities.Worker;
+
+@Service
+public class PaymentService {
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public Payment getPayment(long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", Long.toString(workerId));
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
