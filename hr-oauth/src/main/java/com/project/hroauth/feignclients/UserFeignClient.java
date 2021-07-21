0d7b9a0155e600fca454b128bc5a68c83d2e13e1
Newton Neto <newton.mgs@gmail.com>
2021-07-21 14:46:38 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/project/hroauth/feignclients/UserFeignClient.java
@@ -0,0 +1,16 @@
+package com.project.hroauth.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.hroauth.entities.User;
+
+@Component
+@FeignClient(name = "hr-user", path = "/users")
+public interface UserFeignClient {
+	@GetMapping(value = "/search")
+	ResponseEntity<User> findByEmail(@RequestParam String email);
+}
