0258434725f4ec6c924d5a0def1e45ebf23707bd
Newton Neto <newton.mgs@gmail.com>
2021-07-26 13:42:06 -0300

    zuul como resource server

--- hr-worker/src/main/java/com/project/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/project/hrworker/HrWorkerApplication.java
@@ -2,9 +2,7 @@ package com.project.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
