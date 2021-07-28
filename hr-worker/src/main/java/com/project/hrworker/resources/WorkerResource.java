6698799db0c59e677651fadcbca7a782369fde49
Newton Neto <newton.mgs@gmail.com>
2021-07-28 14:20:37 -0300

    config de banco no config server

--- hr-worker/src/main/java/com/project/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/project/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +22,6 @@ import com.project.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env;
 	
@@ -34,8 +30,6 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
-		
 		return ResponseEntity.noContent().build();
 	}
 	
